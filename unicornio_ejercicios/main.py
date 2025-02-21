e708349864a6b68eb1f19dbe7e260d8c5e83ce81
LuisaBuso <luisafbs3@gmail.com>
2025-02-21 18:39:59 -0500

    Agrego ejercicios Viernes 21

---
+++ unicornio_ejercicios/main.py
@@ -0,0 +1,34 @@
+""" Ejercicio 1: Variables y constantes
+    Declara una variable llamada 'nombre' y asígnale tu nombre.
+    Declara una constante llamada 'PI' y asígnale el valor de 3.14159."""
+
+"""
+ Ejercicio 2: Operaciones básicas
+ Declara dos variables numéricas y realiza las operaciones de suma, resta, multiplicación y división.
+Imprime el resultado de cada operación.
+"""
+
+"""
+     Ejercicio 3: Listas
+    Crea una lista con los números del 1 al 5 y luego agrega el número 6 al final de la lista.
+"""
+"""
+    Ejercicio 4: Diccionarios
+    Crea un diccionario con tres elementos: 'nombre', 'edad' y 'ciudad'. Luego, imprime el valor de 'nombre'.
+"""
+
+
+"""
+    Ejercicio 5: Condicionales
+    Escribe un condicional que imprima "Mayor de edad" si la edad es mayor o igual a 18, y "Menor de edad" en caso contrario.
+"""
+
+"""     Ejercicio 6: Bucles
+        Escribe un bucle que imprima los números del 1 al 10.
+"""
+
+"""
+    Investiga sobre los paradigmas de programación . trae como mínimo 3 
+    ejemplos de paradigmas de programación y explica en qué consisten. 
+    (Pueden hacer ejemplos con código) aplicados a python.
+"""
